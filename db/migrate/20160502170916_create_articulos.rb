a05cbfc72c271a90e702deb9f6cd8aae44f36c0f
Fabian Alejandro Alvarez Ramirez <Fabio.alvarez.ramirez@gmail.com>
2016-05-02 14:10:03 -0300

    crear tabla articulo con su migracion |

---
+++ db/migrate/20160502170916_create_articulos.rb
@@ -0,0 +1,14 @@
+class CreateArticulos < ActiveRecord::Migration
+  def change
+    create_table :articulos do |t|
+      t.string :nombre
+      t.text :descripcion
+      t.integer :stock
+      t.integer :precio_compra
+      t.integer :precio_venta
+      t.image :imagen
+
+      t.timestamps null: false
+    end
+  end
+end
