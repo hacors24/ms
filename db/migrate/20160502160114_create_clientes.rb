8b01a4113bcbb014ef0d3d9ba0127a844e919669
Oscar Maya <oscar.maya1993@gmail.com>
2016-05-02 13:06:16 -0300

    Modelo Cliente con su respectiva migracion

---
+++ db/migrate/20160502160114_create_clientes.rb
@@ -0,0 +1,18 @@
+class CreateClientes < ActiveRecord::Migration
+  def change
+    create_table :clientes do |t|
+      t.string :nombres
+      t.string :apellidos
+      t.date :fecha_nac
+      t.string :razon_social
+      t.string :domicilio
+      t.integer :telefono_fijo
+      t.integer :celular
+      t.string :email
+      t.string :sitio_web
+      t.text :comentario
+
+      t.timestamps null: false
+    end
+  end
+end
