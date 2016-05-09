ad715a21c7e25673052212ff1fed03edfa8e7fee
Fabian Alejandro Alvarez Ramirez <Fabio.alvarez.ramirez@gmail.com>
2016-05-09 14:24:37 -0300

    ADD: CRUD ARTICULO (create, read, update, delete) ADD: template add: gema template
    Signed-off-by: Fabian Alejandro Alvarez Ramirez <Fabio.alvarez.ramirez@gmail.com>

---
+++ app/controllers/articulos_controller.rb
@@ -0,0 +1,62 @@
+class ArticulosController < ApplicationController
+
+	before_action :set_articulo ,only:[:show, :edit,:update, :destroy]
+
+
+	def index
+		@articulos = Articulo.all
+	end
+
+	def edit
+	end
+
+
+	def show 
+	end
+
+
+	def update
+		respond_to do |format|
+			if @articulo.update(articulo_params)
+				format.html{redirect_to @articulo, notice: 'Editado con exito un articulo'}
+			else
+				format.html{render :edit}
+			end
+		end
+	end
+
+	def create
+		@articulo = Articulo.create(articulo_params)
+		respond_to do |format|
+			if @articulo.save
+				format.html{redirect_to @articulo, notice: 'Agregado Articulo con exito'}
+			else
+				format.html{render :new}
+			end
+		end
+
+	end
+
+	def destroy
+		@articulo.destroy
+		respond_to do |format|
+			format.html{redirect_to articulos_url, noice: 'Eliminado con exito'}
+		end
+	end
+
+	def new
+		@articulo = Articulo.new
+	end
+
+
+	private
+
+	def set_articulo
+		@articulo = Articulo.find(params[:id])
+	end
+
+	def articulo_params
+		params.require(:articulo).permit(:nombre, :descripcion,:stock,:precio_venta,:precio_compra,:imagen)
+	end
+
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-<<<<<<< HEAD
   	
   end
-=======
-  end
   
->>>>>>> 7fed5afa8470c91e1e3eb49e565f753546912f65
 end
