c555b898a1ae85209f375a18de7c3cbcc178fabe
Fabian Alejandro Alvarez Ramirez <Fabio.alvarez.ramirez@gmail.com>
2016-05-09 16:16:13 -0300

    CRUD Detalle_Cotizaciones

---
+++ app/controllers/detalle_cotizaciones_controller.rb
@@ -0,0 +1,69 @@
+class DetalleCotizacionesController < ApplicationController
+
+
+before_action :set_detalle_cotizaciones ,only:[:show, :edit,:update, :destroy]
+
+
+	def index
+		@detalle_cotizaciones= Detalle_Cotizaciones.all
+	end
+
+
+	def edit
+	end
+
+
+	def show 
+	end
+
+
+
+
+
+	def update
+		respond_to do |format|
+			if @detalle_cotizacion.update(detalle_cotizacion_params)
+				format.html{redirect_to @detalle_cotizacion, notice: 'Editado con exito el detalle'}
+			else
+				format.html{render :edit}
+			end
+		end
+	end
+
+	def create
+		@detalle_cotizacion = Detalle_Cotizacion.create(detalle_cotizacion_params)
+		respond_to do |format|
+			if @detalle_cotizacion.save
+				format.html{redirect_to @detalle_cotizacion, notice: 'Agregado Detalle con exito'}
+			else
+				format.html{render :new}
+			end
+		end
+
+	end
+
+	def destroy
+		@detalle_cotizacion.destroy
+		respond_to do |format|
+			format.html{redirect_to detalle_cotizacion_url, notice: 'Eliminado Detalle con exito'}
+		end
+	end
+
+	def new
+		@detalle_cotizacion = Detalle_Cotizacion.new
+	end
+
+
+	private
+
+	def set_detalle_cotizacion
+		@detalle_cotizacion= Detalle_Cotizacion.find(params[:id])
+	end
+
+	def detalle_cotizacion_params
+		params.require(:detalle_cotizacion).permit(:cotizacion_id , :articulo_id ,:cantidad,:precio_unitario)
+	end
+
+
+
+end
---
+++ test/controllers/detalle_cotizaciones_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DetalleCotizacionesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
