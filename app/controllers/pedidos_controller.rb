5dac125f8e386d33561c7c70e1a4ac5f962b0f09
Fabian Alejandro Alvarez Ramirez <Fabio.alvarez.ramirez@gmail.com>
2016-05-09 15:59:49 -0300

    CRUD Pedidos

---
+++ app/controllers/pedidos_controller.rb
@@ -0,0 +1,68 @@
+class PedidosController < ApplicationControllerrails 
+
+before_action :set_pedidos ,only:[:show, :edit,:update, :destroy]
+
+
+	def index
+		@pedidos = Pedido.all
+	end
+
+
+	def edit
+	end
+
+
+	def show 
+	end
+
+
+
+
+
+	def update
+		respond_to do |format|
+			if @pedido.update(pedido_params)
+				format.html{redirect_to @pedido, notice: 'Editado con exito el pedido'}
+			else
+				format.html{render :edit}
+			end
+		end
+	end
+
+	def create
+		@pedido = Pedido.create(pedido_params)
+		respond_to do |format|
+			if @pedido.save
+				format.html{redirect_to @pedido, notice: 'Agregado Pedido con exito'}
+			else
+				format.html{render :new}
+			end
+		end
+
+	end
+
+	def destroy
+		@pedido.destroy
+		respond_to do |format|
+			format.html{redirect_to pedidos_url, notice: 'Eliminado Pedido con exito'}
+		end
+	end
+
+	def new
+		@pedido = Pedido.new
+	end
+
+
+	private
+
+	def set_pedido
+		@pedido = Pedido.find(params[:id])
+	end
+
+	def pedido_params
+		params.require(:pedido).permit(:cliente_id , :fecha_pedido ,:fecha_entrega,:estado,:descripcion)
+	end
+
+
+
+end
--- app/controllers/ventas_controller.rb
+++ app/controllers/ventas_controller.rb
@@ -1,2 +1,68 @@
 class VentasController < ApplicationController
+
+
+before_action :set_ventas ,only:[:show, :edit,:update, :destroy]
+
+
+	def index
+		@ventas = Venta.all
+	end
+
+
+	def edit
+	end
+
+
+	def show 
+	end
+
+
+
+
+
+	def update
+		respond_to do |format|
+			if @venta.update(venta_params)
+				format.html{redirect_to @venta, notice: 'Editado con exito la venta'}
+			else
+				format.html{render :edit}
+			end
+		end
+	end
+
+	def create
+		@venta = Venta.create(venta_params)
+		respond_to do |format|
+			if @venta.save
+				format.html{redirect_to @venta, notice: 'Agregada Venta con exito'}
+			else
+				format.html{render :new}
+			end
+		end
+
+	end
+
+	def destroy
+		@venta.destroy
+		respond_to do |format|
+			format.html{redirect_to ventas_url, notice: 'Eliminada Venta con exito'}
+		end
+	end
+
+	def new
+		@venta = Venta.new
+	end
+
+
+	private
+
+	def set_venta
+		@venta = Venta.find(params[:id])
+	end
+
+	def venta_params
+		params.require(:venta).permit(:cliente_id , :fecha_acotizacion,:fecha_tcotizacion,:descuento,:descripcion)
+	end
+
+
 end
