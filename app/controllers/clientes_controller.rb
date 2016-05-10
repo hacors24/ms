3d5d9e2d336be8e27265c0f81df83d2d7b5fc017
Fabian Alejandro Alvarez Ramirez <Fabio.alvarez.ramirez@gmail.com>
2016-05-10 00:46:51 -0300

    vista Clientes

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,6 +1,6 @@
 class ClientesController < ApplicationController
 
-before_action :set_articulo ,only:[:show, :edit,:update, :destroy]
+before_action :set_cliente ,only:[:show, :edit,:update, :destroy]
 
 
 	def index
