8579181690e263bca154e2ba94028781e46a65c2
Oscar Maya <oscar.maya1993@gmail.com>
2016-07-18 19:41:45 -0400

    Arreglos en login y adminlte
    Signed-off-by: Oscar Maya <oscar.maya1993@gmail.com>

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,6 +1,22 @@
 class Articulo < ActiveRecord::Base
 	has_attached_file :imagen2 ,styles: {thumb:"800x600"}
 	validates_attachment_content_type :imagen2, :content_type => /\Aimage\/.*\Z/
-	
+	validates :nombre, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Solo puede ingresar letras" } 
+
+    validates :descripcion, length: { maximum: 500 }
+    #validates :mayor_cero
+    validates :stock, :mayor_cero, numericality: { only_integer: true, :message => "Solo puede ingresar numeros"}
+    #falta que sea mayor a 0
+ 
+    validates :precio_compra, numericality: { only_integer: true, message: "Solo puede ingresar numeros"}
+    validates :precio_venta, numericality: { only_integer: true, message: "Solo puede ingresar numeros"}
+   
+    def mayor_cero
+    	if stock < 0
+    		errors.add(:stock, "Ingresa un stock positivo")
+    	end
+    end
+
 
 end
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,4 +2,8 @@ class Cliente < ActiveRecord::Base
 	has_many :pedidos, :dependent => :delete_all
 	has_many :ventas, :dependent => :delete_all
 	has_many :cotizaciones, :dependent => :delete_all
+    
+    
+
+
 end
